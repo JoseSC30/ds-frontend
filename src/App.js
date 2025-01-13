3c35436efa6f0c052dd6655183d58e3bc8c4ce95
joseluis <joseluis.universidad2020@gmail.com>
2025-01-12 20:20:47 -0400

    Revisiones 1

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import Principal from './components/Principal';
 import Login from './components/Login';
 import Register from './components/Register';
 
-
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route className="App" path="/diagrama/:nombreDiagrama/:coleccion/:existe" element={<DiagramaClases />} />
+        <Route path="/diagrama/:nombreDiagrama/:coleccion/:existe" element={<DiagramaClases />} />
         <Route path="/principal" element={<Principal />} />
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
